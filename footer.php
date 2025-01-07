9d9c80f7ccde44d7845459b581ebe4a07367acc0
markusvitru <markusvitru@gmail.com>
2025-01-07 23:32:22 +0100

    Se termina maquetacion con algunos faltantes

--- footer.php
+++ footer.php
@@ -4,7 +4,50 @@
 */
 ?>
     <footer>
-      <p>Copyright &copy; <?php echo date("Y"); ?></p>
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo-footer">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="Logo" class="img-responsive">
+      </a>
+      <div class="menus-footer">
+        <p class="title-menu-footer s-color">iRCONGAS Energías<br>Alternativas Autmoción SL</p>
+        <p>C/De la Tecnología. 5 P.I Los Olivos<br>28906 Getafe (Madrid)<br>91 468 31 17<br>info@ircongas.com</p>
+        <span class="s-color">Empresa certtificada por</span><br>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/logo-uca.png" alt="Logo UCA" class="img-responsive">
+      </div>
+      <div class="menus-footer">
+        <p class="title-menu-footer s-color">Principales marcas</p>
+        <ul>
+          <li><a href="#">Menu 1</a></li>
+          <li><a href="#">Menu 2</a></li>
+          <li><a href="#">Menu 3</a></li>
+          <li><a href="#">Menu 4</a></li>
+          <li><a href="#">Menu 5</a></li>
+          <li><a href="#">Menu 6</a></li>
+        </ul>
+      </div>
+      <div class="menus-footer">
+        <p class="title-menu-footer s-color">Principales modelos</p>
+        <ul>
+          <li><a href="#">Menu 1</a></li>
+          <li><a href="#">Menu 2</a></li>
+          <li><a href="#">Menu 3</a></li>
+          <li><a href="#">Menu 4</a></li>
+          <li><a href="#">Menu 5</a></li>
+          <li><a href="#">Menu 6</a></li>
+          <li><a href="#">Menu 7</a></li>
+        </ul>
+      </div>
+      <div class="menus-footer">
+        <p class="title-menu-footer s-color">Servicios</p>
+        <ul>
+          <li><a href="#">Menu 1</a></li>
+          <li><a href="#">Menu 2</a></li>
+          <li><a href="#">Menu 3</a></li>
+          <li><a href="#">Menu 4</a></li>
+          <li><a href="#">Menu 5</a></li>
+          <li><a href="#">Menu 6</a></li>
+          <li><a href="#">Menu 7</a></li>
+        </ul>
+      </div>
     </footer>
     <?php wp_footer(); ?>
   </body>
--- template-parts/home.php
+++ template-parts/home.php
@@ -21,7 +21,7 @@
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing.
             </p>
             <span class="title-model">MODELO (MODELO)</span>
-            <div>
+            <div class="description-model">
                 <div class="double f-left">
                     <img src="<?php echo get_template_directory_uri(); ?>/images/car-model.png" alt="Vehículo modelo" class="img-responsive">
                 </div>
@@ -41,34 +41,83 @@
                     <br><span class="g-color">HM01</span>
                 </div>
 
-                <div class="normal-content f-left">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-motor.png" alt="Vehículo modelo" class="img-responsive">
+                <div class="normal-content f-left m-top">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-inyeccion.png" alt="Vehículo modelo" class="img-responsive">
                     <br><span class="s-color">INYECCIÓN</span>
                     <br><span class="g-color">Indirecta</span>
                 </div>
-                <div class="normal-content f-left">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-motor.png" alt="Vehículo modelo" class="img-responsive">
+                <div class="normal-content f-left m-top">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-glp.png" alt="Vehículo modelo" class="img-responsive">
                     <br><span class="s-color">SISTEMA GLP<br>INSTALADO</span>
                     <br><span class="g-color">kit GLP Europegas<br>Premo Avance 32</span>
                 </div>
-                <div class="normal-content f-left">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-motor.png" alt="Vehículo modelo" class="img-responsive">
+                <div class="normal-content f-left m-top">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-deposito.png" alt="Vehículo modelo" class="img-responsive">
                     <br><span class="s-color">CAP. DEPÓSITO</span>
                     <br><span class="g-color">Toroidal interno 600x200<br>42L/33,6L</span>
                 </div>
-                <div class="normal-content f-left">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-motor.png" alt="Vehículo modelo" class="img-responsive">
+                <div class="normal-content f-left m-top">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-autonomia.png" alt="Vehículo modelo" class="img-responsive">
                     <br><span class="s-color">AUTONOMÍA</span>
                     <br><span class="g-color">600Km</span>
                 </div>
-                <div class="normal-content f-left">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-motor.png" alt="Vehículo modelo" class="img-responsive">
+                <div class="normal-content f-left m-top">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-carga.png" alt="Vehículo modelo" class="img-responsive">
                     <br><span class="s-color">TOMO DE CARGA</span>
                     <br><span class="g-color">Italiana pequeña<br>Adaptador Euro unificado</span>
                 </div>
             </div>
         </div>
+        <div class="section-black">
+            <p class="s-color">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+            </p>
+            <p class="b-color">
+                Adapta tu vehículo a GLP y comienza a disfrutar de todas las ventajas de la etiqueta ECO
+            </p>
+            <div class="content-btn-init">
+                <a href="#" class="btn-init">Comenzar</a>
+            </div>
+        </div>
+        <div class="request">
+            <p class="title-request">
+                Lorem Ipsum is simply
+            </p>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing.<br><br>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy
+            </p>
+            <div class="content-btn-center">
+                <a href="#" class="btn-init">Solicitar presupuesto</a>   |   <span>+34 666 66 66 66</span>
+            </div>
+        </div>
+        <div class="request-img t-right">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/estimate.png" alt="Vehículo solicitar presupuesto" class="img-responsive">
+        </div>
+        <div class="text-top">
+            <p class="title-text-top s-color">
+                Lorem Ipsum is simply
+            </p>
+            <p class="parragraph-text-top g-color">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing.<br><br>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Ipsum is simply dummy
+            </p>
+        </div>
     </div>
     <div class="section-right">
+    </div>
+    <div class="logos">
+        <div class="content-logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/logo-stag.png" alt="Vehículo solicitar presupuesto" class="img-responsive">
+        </div>
+        <div class="content-logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/logo-rotarex.png" alt="Vehículo solicitar presupuesto" class="img-responsive">
+        </div>
+        <div class="content-logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/logo-tugra.png" alt="Vehículo solicitar presupuesto" class="img-responsive">
+        </div>
+        <div class="content-logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/logo-europegas.png" alt="Vehículo solicitar presupuesto" class="img-responsive">
+        </div>
     </div>
 </div>
