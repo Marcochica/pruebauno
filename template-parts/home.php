b76bccec68ba8ee22b2a0e68beb4142930f27286
markusvitru <markusvitru@gmail.com>
2025-01-07 21:11:17 +0100

    Avance seccion central pagina

--- template-parts/home.php
+++ template-parts/home.php
@@ -6,14 +6,68 @@
         </div>
     </div>
     <div class="start-now">
-        <div>
-            <span class="small-start-now"></span>
-            <span class="big-start-now"></span>
-            <span class="medium--start-now"></span>
+        <div class="content-text-start-now">
+            <span class="small-start-now">Lorem Ipsum is simply dummy</span><br>
+            <span class="big-start-now">Lorem Ipsum is simply dummy</span><br>
+            <span class="medium-start-now">Lorem Ipsum is simply dummy text of the printing and <br>typesetting industry.</span>
+        </div>
+        <div class="content-btn-start-now">
+            <a href="#" class="btn-start-now">¡Comenzar ya!</a>
         </div>
     </div>
     <div class="section-left">
-        
+        <div class="content-model">
+            <p class="text-model">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing.
+            </p>
+            <span class="title-model">MODELO (MODELO)</span>
+            <div>
+                <div class="double f-left">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/car-model.png" alt="Vehículo modelo" class="img-responsive">
+                </div>
+                <div class="normal-content f-left">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-cilindrada.png" alt="Vehículo modelo" class="img-responsive">
+                    <br><span class="s-color">CILINDRADA</span>
+                    <br><span class="g-color">1199cc/ 3 Cil.</span>
+                </div>
+                <div class="normal-content f-left">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-voltaje.png" alt="Vehículo modelo" class="img-responsive">
+                    <br><span class="s-color">kW /CV</span>
+                    <br><span class="g-color">60kW | 82cv</span>
+                </div>
+                <div class="normal-content f-left">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-motor.png" alt="Vehículo modelo" class="img-responsive">
+                    <br><span class="s-color">COD. MOTOR</span>
+                    <br><span class="g-color">HM01</span>
+                </div>
+
+                <div class="normal-content f-left">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-motor.png" alt="Vehículo modelo" class="img-responsive">
+                    <br><span class="s-color">INYECCIÓN</span>
+                    <br><span class="g-color">Indirecta</span>
+                </div>
+                <div class="normal-content f-left">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-motor.png" alt="Vehículo modelo" class="img-responsive">
+                    <br><span class="s-color">SISTEMA GLP<br>INSTALADO</span>
+                    <br><span class="g-color">kit GLP Europegas<br>Premo Avance 32</span>
+                </div>
+                <div class="normal-content f-left">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-motor.png" alt="Vehículo modelo" class="img-responsive">
+                    <br><span class="s-color">CAP. DEPÓSITO</span>
+                    <br><span class="g-color">Toroidal interno 600x200<br>42L/33,6L</span>
+                </div>
+                <div class="normal-content f-left">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-motor.png" alt="Vehículo modelo" class="img-responsive">
+                    <br><span class="s-color">AUTONOMÍA</span>
+                    <br><span class="g-color">600Km</span>
+                </div>
+                <div class="normal-content f-left">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icono-motor.png" alt="Vehículo modelo" class="img-responsive">
+                    <br><span class="s-color">TOMO DE CARGA</span>
+                    <br><span class="g-color">Italiana pequeña<br>Adaptador Euro unificado</span>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="section-right">
     </div>
